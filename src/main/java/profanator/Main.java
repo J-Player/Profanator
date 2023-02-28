ee90e51316a5d44c744088e6178327eb6ff4687b
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-02-28 19:10:20 -0300

    Classe principal (Main) foi simplificada.

--- src/main/java/profanator/Main.java
+++ src/main/java/profanator/Main.java
@@ -1,7 +1,14 @@
 package profanator;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import profanator.controllers.MainControllerFX;
 
 /**
  * <p>Autor: João Pedro R. Diniz</p>
@@ -10,10 +17,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <p>Versão atual: 0.0.1</p>
  */
 @SpringBootApplication
-public class Main {
+public class Main extends Application {
+
+    private ConfigurableApplicationContext context;
+
+    @Override
+    public void init() {
+        context = new SpringApplicationBuilder(getClass())
+                .run(getParameters().getRaw().toArray(new String[0]));
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/index.fxml"));
+        Parent root = loader.load();
+        MainControllerFX controller = loader.getController();
+        controller.setStage(stage);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        context.close();
+    }
 
     public static void main(String[] args) {
-        Application.launch(JavaFXApplication.class, args);
+        launch(args);
     }
 
 }
