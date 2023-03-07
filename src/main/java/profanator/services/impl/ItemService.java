9767d528b7871f8fcd547fc5fdd1eba6e649adb3
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-03-06 23:00:22 -0300

    Classe ItemService atualizada.

--- src/main/java/profanator/services/impl/ItemService.java
+++ src/main/java/profanator/services/impl/ItemService.java
@@ -8,15 +8,23 @@ import profanator.services.IService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ItemService implements IService<Item> {
 
     private final ItemRepository repository;
 
+    private final IngredientService ingredientService;
+
     @Override
     public Mono<Item> findById(Long id) {
         return repository.findById(id);
+    }
+
+    public Mono<Item> findByName(String name) {
+        return repository.findByName(name);
     }
 
     @Override
@@ -26,6 +34,15 @@ public class ItemService implements IService<Item> {
 
     public Flux<Item> findAllByProficiency(String proficiency) {
         return repository.findByProficiency(proficiency);
+    }
+
+    public Mono<List<Item>> getIngredients(String product) {
+        return ingredientService.findByProduct(product)
+                .flatMap(ingredient -> findByName(ingredient.getName())
+                        .map(item -> item.withQuantity(ingredient.getQuantity())))
+                .flatMap(item -> getIngredients(item.getName())
+                        .map(item::withIngredients))
+                .collectList();
     }
 
 }
