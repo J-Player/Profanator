d6eb9289fb22c3c44f9c298c0d2458296ccda0a2
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-03-21 18:00:25 -0300

    Builder annotation adicionada à Classe.

--- src/main/java/profanator/domains/Ingredient.java
+++ src/main/java/profanator/domains/Ingredient.java
@@ -1,11 +1,13 @@
 package profanator.domains;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table
