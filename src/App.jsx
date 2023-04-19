a4d0605899835244b2a6f6eaa3f466ed31c4235d
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-04-19 18:29:09 -0300

    App.jsx e app.scss foram atualizados.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router-dom"
 
-import Footer from './components/Footer'
-
 import './app.scss'
 
 function App() {
   return (
     <div id="app">
       <Outlet/>
-      <Footer/>
     </div>
   )
 }
