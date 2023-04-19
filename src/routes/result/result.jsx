82034006da7d55382f704f2882a9f5610151ae1e
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-04-19 18:34:28 -0300

    result.jsx e result.scss aprimorados.

--- src/routes/result/result.jsx
+++ src/routes/result/result.jsx
@@ -1,137 +1,47 @@
 import { useState } from 'react'
-import { calculate } from '../../utils/index'
 
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import './result.scss'
 
 function TreeItem (props) {
-    const [open, setOpen] = useState(true)
+    const [showIngredients, setShowIngredients] = useState(true)
     const {name, quantity, restQt, ingredients} = props.item
     let label = `${name} x ${quantity}`
     if (restQt) label = label.concat(` [rest: ${restQt}]`)
+    const srcImage = `images/items/${name.toLowerCase().replace(' ', '_')}.png`
     return (
-        <div className='item'>
-            <img src="images/items/ambar1.png" alt=""/>
-            <span onClick={e => setOpen(!open)}>{open ? label : <del>{label}</del>}</span>
+        <div className='item-container'>
+            <div className='item' onClick={e => setShowIngredients(!showIngredients)}>
+                <div className='item-image'><img src={srcImage} onError={e => e.target.style.display = 'none'} /></div>
+                <span>{showIngredients ? label : <del>{label}</del>}</span>
+            </div>
             <div className='list-item'>
-                {open && ingredients?.map((item, index) => <TreeItem key={index} item={item} />)}
+                {showIngredients && ingredients?.map((item, index) => <TreeItem key={index} item={item} />)}
             </div>
         </div>
     )
 }
-
-function TreeView (props) {
-    return (props.tree.map((item, i) => <TreeItem key={i} item={item}/>))
-}
-
-function Result (props) {
-
-    const item = {
-        'id': 35,
-        'proficiency': 'Architecture',
-        'name': 'Small Ship',
-        'qtByProduction': 1,
-        'ingredients': [
-            {
-                'proficiency': 'Blacksmith',
-                'name': 'Anchor',
-                'qtByProduction': 1,
-                'quantity': 1,
-                'ingredients': [
-                    {
-                        'name': 'Iron Ingot',
-                        'quantity': 10,
-                        'ingredients': [{ 'name': 'Rough Iron', 'quantity': 2 }]
-                    }
-                ]
-            },
-            {
-                'proficiency': 'Tinkering',
-                'name': 'Iron Nails',
-                'qtByProduction': 3,
-                'quantity': 60,
-                'ingredients': [
-                    {
-                        'name': 'Iron Ingot',
-                        'quantity': 1,
-                        'ingredients': [{ 'name': 'Rough Iron', 'quantity': 2 }]
-                    }
-                ]
-            },
-            {
-                'proficiency': 'Tailoring',
-                'name': 'Line Cloth',
-                'qtByProduction': 1,
-                'quantity': 80,
-                'ingredients': [{ 'name': 'Plant Fiber', 'quantity': 5 }]
-            },
-            {
-                'proficiency': 'Tailoring',
-                'name': 'Rope',
-                'qtByProduction': 1,
-                'quantity': 30,
-                'ingredients': [{ 'name': 'Plant Fiber', 'quantity': 2 }]
-            },
-            {
-                'proficiency': 'Carpentry',
-                'name': 'Steering Wheel',
-                'qtByProduction': 1,
-                'quantity': 1,
-                'ingredients': [
-                    {
-                        'name': 'Iron Ingot',
-                        'quantity': 2,
-                        'ingredients': [{ 'name': 'Rough Iron', 'quantity': 2 }]
-                    },
-                    {
-                        'proficiency': 'Tinkering',
-                        'name': 'Iron Nails',
-                        'qtByProduction': 3,
-                        'quantity': 10,
-                        'ingredients': [
-                            {
-                                'name': 'Iron Ingot',
-                                'quantity': 1,
-                                'ingredients': [
-                                    { 'name': 'Rough Iron', 'quantity': 2 }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        'proficiency': 'Tailoring',
-                        'name': 'Rope',
-                        'qtByProduction': 1,
-                        'quantity': 1,
-                        'ingredients': [{ 'name': 'Plant Fiber', 'quantity': 2 }]
-                    },
-                    { 'name': 'Stone', 'quantity': 4 },
-                    { 'name': 'Wood', 'quantity': 8 }
-                ]
-            },
-            {
-                'proficiency': 'Carpentry',
-                'name': 'Wood Plank',
-                'qtByProduction': 1,
-                'quantity': 170,
-                'ingredients': [{ 'name': 'Wood', 'quantity': 4 }]
-            }
-        ]
-    }   
-
-    item.quantity = 7
-
-    calculate(item)
-
-    return (
-    <div className='container'>
-        <div className='result-container'>
-            <div id='result-tree-view'><TreeView tree={[item]}/></div>
-            <div><Link to='/'>Back</Link></div>
-        </div>
-    </div>
-    )
-}
-
+    
+    function TreeView (props) {
+        return (props.tree.map((item, i) => <TreeItem key={i} item={item}/>))
+    }
+    
+    function Result() {
+        const location = useLocation()
+        const item = location.state.data.item
+        return (
+            <div className='container'>
+                <div className='main-container'>
+                    <div className='tree-view-container'>
+                    <TreeView tree={[item]}/>
+                    </div>
+                    <div>
+                        <button><Link to='/'>Back</Link></button>
+                    </div>
+                </div>
+            </div>
+            )
+        }
+        
 export default Result
